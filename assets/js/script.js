04957c8b82fb740ef0713819a38a93b55be0756c
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-05-01 16:08:06 -0400

    added 5 day

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,126 +1,138 @@
+var myKey = '9fb25e682537903c979f563799d58c09';
+var myLat = '';
+var myLon = '';
+var myCity = '';
+var cardDate = '';
+var cardIcon = '';
+var cardTemp = '';
+var cardRh = '';
+var cardIdent = '';
+var iconLogo = '';
 
-var myKey = '9fb25e682537903c979f563799d58c09'
-var myLat = ''
-var myLon = ''
-var myCity = ''
-var card = $('.card-body');
-var cardDate = ''
-var cardIcon = ''
-var cardTemp = ''
-var cardRh = ''
-// var myZip = $('.zip').val();
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude=minutely,hourly,alerts&units=imperial&appid={API key}
-
-function fetchApi () {
-    fetch('https://api.openweathermap.org/data/2.5/onecall?lat=' + myLat +'&lon=' + myLon + '&exclude=minutely,hourly,alerts&units=imperial&appid=' + myKey)
+function fetchApi() {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/onecall?lat=' +
+      myLat +
+      '&lon=' +
+      myLon +
+      '&exclude=minutely,hourly,alerts&units=imperial&appid=' +
+      myKey
+  )
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
-        var cityNameDate = document.createElement('h1');
-        var jumboTemp = document.createElement('h2');
-        var jumboRH = document.createElement('h2');
-        var jumboWind = document.createElement('h2');
-        var jumboUvi = document.createElement('h2');
-        cityNameDate.textContent = myCity + ' ' + moment().format("MMM Do YYYY");
-        jumboTemp.textContent = 'Current Temp: ' + data.current.temp + ' F';
-        jumboRH.textContent = 'Humidity: ' + data.current.humidity + ' %';
-        jumboWind.textContent = 'Wind Speed: ' + data.current.wind_speed + ' mph';
-        jumboUvi.textContent = 'UV Index: ' + data.current.uvi;
-        $('.conditions').append(cityNameDate);
-        $('.conditions').append(jumboTemp);
-        $('.conditions').append(jumboRH);
-        $('.conditions').append(jumboWind);
-        $('.conditions').append(jumboUvi);
-        
-        for (var i = 0; i < 6; i++) {
-            cardDate = document.createElement('h4');
-            cardIcon = document.createElement('h4');
-            cardTemp = document.createElement('h4');
-            cardRh = document.createElement('h4');
-            cardDate.textContent = moment().format("MMM Do YYYY");
-            // cardIcon.innerhtml = data.daily.weather.icon;
-            cardTemp.textContent = data[i].daily.temp.max;
-            cardRh.textContent = data[i].daily.humidity;
-            card.append(cardDate);
-            card.append(cardIcon);
-            card.append(cardTemp);
-            card.append(cardRh);
+      var cardData = data
+      var cityNameDate = document.createElement('h1');
+      var jumboTemp = document.createElement('h2');
+      var jumboRH = document.createElement('h2');
+      var jumboWind = document.createElement('h2');
+      var jumboUvi = document.createElement('h2');
+      cityNameDate.textContent = myCity + ' ' + moment().format('MMM Do YYYY');
+      jumboTemp.textContent = 'Current Temp: ' + data.current.temp + ' F';
+      jumboRH.textContent = 'Humidity: ' + data.current.humidity + ' %';
+      jumboWind.textContent = 'Wind Speed: ' + data.current.wind_speed + ' mph';
+      jumboUvi.textContent = 'UV Index: ' + data.current.uvi;
+      $('.conditions').append(cityNameDate);
+      $('.conditions').append(jumboTemp);
+      $('.conditions').append(jumboRH);
+      $('.conditions').append(jumboWind);
+      $('.conditions').append(jumboUvi);
+      console.log(cardData);
 
-        }
+      $('.card-body').each(function() {
+        cardIdent = $(this).attr('data-card');
+        cardDate = document.createElement('h4');
+        cardIcon = document.createElement('img');
+        cardTemp = document.createElement('h4');
+        cardRh = document.createElement('h4');
+        cardDate.textContent = moment().add(cardIdent, 'days').calendar('MMM Do YYYY');
+        console.log(cardData.daily[cardIdent].weather[0].icon)
+        iconLogo = cardData.daily[cardIdent].weather[0].icon;
+        $('img').attr('src', 'http://openweathermap.org/img/wn/' + iconLogo + '@2x.png');
+        cardTemp.textContent = cardData.daily[cardIdent].temp.max + ' F';
+        cardRh.textContent = cardData.daily[cardIdent].humidity + ' %';
+        $(this).append(cardDate);
+        $(this).append(cardIcon);
+        $(this).append(cardTemp);
+        $(this).append(cardRh);
+     })
+      
     })
 }
 
-function newYork () {
-    myLat = '40.71';
-    myLon = '-74.01';
-    myCity = 'New York City, Ny';
-    fetchApi();
+function newYork() {
+  myLat = '40.71';
+  myLon = '-74.01';
+  myCity = 'New York City, NY';
+  fetchApi();
 }
-
-function losAngeles () {
-    myLat = '34.05';
-    myLon = '-118.24';
-    myCity = 'Los Angeles, Ca';
-    fetchApi();
+function losAngeles() {
+  myLat = '34.05';
+  myLon = '-118.24';
+  myCity = 'Los Angeles, CA';
+  fetchApi();
 }
-function chicago () {
-    myLat = '41.87';
-    myLon = '-87.62';
-    myCity = 'Chicago, Il';
-    fetchApi();
+function chicago() {
+  myLat = '41.87';
+  myLon = '-87.62';
+  myCity = 'Chicago, IL';
+  fetchApi();
 }
-function houston () {
-    myLat = '29.76';
-    myLon = '-95.36';
-    myCity = 'Houston, Tx';
-    fetchApi();
+function houston() {
+  myLat = '29.76';
+  myLon = '-95.36';
+  myCity = 'Houston, TX';
+  fetchApi();
 }
-function phoenix () {
-    myLat = '33.44';
-    myLon = '-112.07';
-    myCity = 'Phoenix, Az';
-    fetchApi();
+function phoenix() {
+  myLat = '33.44';
+  myLon = '-112.07';
+  myCity = 'Phoenix, AZ';
+  fetchApi();
 }
-function philadelphia () {
-    myLat = '39.95';
-    myLon = '-75.16';
-    myCity = 'Philadelphia, Pa';
-    fetchApi();
+function philadelphia() {
+  myLat = '39.95';
+  myLon = '-75.16';
+  myCity = 'Philadelphia, PA';
+  fetchApi();
 }
-function sanAntonio () {
-    myLat = '29.42';
-    myLon = '-98.49';
-    myCity = 'San Antonio, Tx';
-    fetchApi();
+function sanAntonio() {
+  myLat = '29.42';
+  myLon = '-98.49';
+  myCity = 'San Antonio, TX';
+  fetchApi();
 }
-function sanDiego () {
-    myLat = '32.71';
-    myLon = '-117.16';
-    myCity = 'San Diego, Ca';
-    fetchApi();
+function sanDiego() {
+  myLat = '32.71';
+  myLon = '-117.16';
+  myCity = 'San Diego, CA';
+  fetchApi();
 }
 function zipCode() {
-var myZip = $('.zip').val();
-    fetch('http://api.openweathermap.org/geo/1.0/zip?zip=' + myZip + ',US&appid=' + myKey) 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            myLat = data.lat;
-            myLon = data.lon;
-            myCity = data.name;
-        fetchApi();
-        })
+  var myZip = $('.zip').val();
+  fetch(
+    'http://api.openweathermap.org/geo/1.0/zip?zip=' +
+      myZip +
+      ',US&appid=' +
+      myKey
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      myLat = data.lat;
+      myLon = data.lon;
+      myCity = data.name;
+      fetchApi();
+    });
+}
+function clearForm() {
+  $('.conditions').empty();
+  $('.card-body').empty();
 }
 
-
-function clearForm () {
-    $('.conditions').empty();
-    $('.card-body').empty();
-    card.empty();
-}
 $('.btn').on('click', clearForm);
 $('.zip-btn').on('click', zipCode);
 $('.ny').on('click', newYork);
@@ -133,6 +145,4 @@ $('.sa').on('click', sanAntonio);
 $('.sd').on('click', sanDiego);
 
 //TODO:
-//5 Day Forecast
 //Local Storage
-
